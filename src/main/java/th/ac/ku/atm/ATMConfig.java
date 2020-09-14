7494e519cc4dfc9810a609bfa02c1378dfdb049d
kunlachart promfang <kunlachart.p@ku.th>
2020-09-14 19:01:34 +0700

    code-config

---
+++ src/main/java/th/ac/ku/atm/ATMConfig.java
@@ -0,0 +1,26 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ATMConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        return new DataSourceFile("text1.txt");
+    }
+
+    @Bean
+    public Bank bank(){
+        return new Bank("My bank",dataSource());
+
+    }
+
+    @Bean
+    public ATM atm(){
+        return new ATM(bank());
+    }
+
+
+}
